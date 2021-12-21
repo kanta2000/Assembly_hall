99005b16275cce73f6cf2e664cb3530e6f86d9a1
kanta2000 <n.kanta0619@icloud.com>
2021-12-21 09:06:02 +0000

    add

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = '915938c9dcec515a06e68635dc2f0342716234914b6f88bdd1aea277a4a0c9a414cdd4233d52f5ac6b97f94f909230141baa0af10f122055eca73954ef773f36'
