33d36322fdef7fa7ad64215c87b7d8661340e206
EC2 Default User <ec2-user@ip-172-31-0-248.ap-northeast-1.compute.internal>
2022-01-04 15:18:14 +0000

    add

--- db/schema.rb
+++ db/schema.rb
@@ -19,15 +19,6 @@ ActiveRecord::Schema.define(version: 2021_12_21_164505) do
     t.datetime "updated_at", null: false
   end
 
-  create_table "likes", force: :cascade do |t|
-    t.integer "post_id"
-    t.integer "user_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["post_id"], name: "index_likes_on_post_id"
-    t.index ["user_id"], name: "index_likes_on_user_id"
-  end
-
   create_table "post_comments", force: :cascade do |t|
     t.text "comment"
     t.integer "user_id"
